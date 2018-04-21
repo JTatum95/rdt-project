b5559dceb122b01a7eed859cc01e4ea7e348c67f
JTatum95 <tateyjj@CousQuick.hsd1.il.comcast.net>
2018-04-21 16:51:20 -0500

    Establish basic format for data transfer

--- rdt.py
+++ rdt.py
@@ -3,28 +3,51 @@ import threading
 import collections
 import queue
 from network import *
+import random
 
 IPPROTO_RDT = 0xfe
 
 class RDTSocket(StreamSocket):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.port = None
+        self.addr = None
+        ports = []
+        addrs = []
         # Other initialization here
 
-    def bind(self, port):
+    def bind(self, port): # Assigns port to socket
+        # Check if available
+        self.port = port
+
+    def listen(self): # Waits for a connection
         pass
 
-    def listen(self):
+    def accept(self): # Clones socket with open port
         pass
 
-    def accept(self):
-        pass
+    def connect(self, addr): # Tell a server you want to connect
+        if self.port == None:
+                self.port = self.random_port() # Bind random
+        self.addr = addr
 
-    def connect(self, addr):
-        pass
+    def send(self, data): # Tell client to recieve from you
+        # Check connected
+        # Send if able
+        self.output(data, self.addr)
+            # Wait for ACK
 
-    def send(self, data):
-        pass
+    def input(self, data, src): # Tell proto that someone is connecting
+        self.deliver(data, src)
+
+    def sendto(self, msg, dst):
+        # Extract header then send
+        self.output(msg, dst)
+
+    def random_port():
+        num = random.randint(1024, 8000)
+        # check if num is taken
+        return num
 
 class RDTProtocol(Protocol):
     PROTO_ID = IPPROTO_RDT
@@ -32,7 +55,14 @@ class RDTProtocol(Protocol):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.sock = super().socket() 
         # Other initialization here
+        # Do multiplexing things
+
+    def socket(self):
+        # Check if socket is in use, get unused
+        return self.sock
 
     def input(self, seg, rhost):
         pass
+        # self.sock.input(seg, src)
